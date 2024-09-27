003594398091158e08c2bc5fc4a393b6ca82509d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2024-09-27 03:10:28 +0000

    Wrap the `App` in a `GraphQLProvider`
    Extract the chain ID, application ID, client service address and the
    owner from the URL.

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    useParams,
+    useSearchParams,
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import GraphQLProvider from './GraphQLProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,11 +17,50 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path=":chainId" element={<GraphQLApp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function GraphQLApp() {
+    const { chainId } = useParams();
+    const [searchParams] = useSearchParams();
+
+    let app = searchParams.get("app");
+    let owner = searchParams.get("owner");
+    let host = searchParams.get("host");
+    let port = searchParams.get("port");
+
+    if (chainId == null) {
+        throw Error("The URL is missing the chain ID");
+    }
+    if (app == null) {
+        throw Error("The URL is missing an `app` query parameter with the application ID");
+    }
+    if (owner == null) {
+        throw Error("The URL is missing an `owner` query parameter with the owner ID");
+    }
+    if (host == null) {
+        console.log(
+            "WARN: the URL does not have a `host` query paramater with the address of the local " +
+            "node service. Assuming it is `localhost`."
+        );
+        host = "localhost";
+    }
+    if (port == null) {
+        console.log(
+            "WARN: the URL does not have a `port` query paramater with the port of the local " +
+            "node service. Assuming it is `8080`."
+        );
+        port = "8080";
+    }
+
+    return (
+        <GraphQLProvider chainId={chainId} applicationId={app} host={host} port={port}>
+            <App />
+        </GraphQLProvider>
+    );
+}
