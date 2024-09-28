f9d53b3ed3923f3b178960695b4052b16bb87a51
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2024-09-28 19:14:11 +0000

    Create an `AccountProvider` helper component
    Injects a `userAccount` property in its children. The actual
    communication with the wallet was based on an online tutorial[1].
    
    [1]: https://hackmd.io/@VydelHxmR0CbKxAe4TjhbQ/BJ9Bf4XD6

---
+++ web-frontend/src/AccountProvider.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { useSyncProviders } from './hooks/useSyncProviders';
+
+type AccountProviderProps = {
+    children: any,
+};
+
+function AccountProvider({ children }: AccountProviderProps) {
+    const [selectedProvider, setSelectedProvider] = useState<EIP6963ProviderDetail>();
+    const [userAccount, setUserAccount] = useState<string | null>(null);
+    const providers = useSyncProviders();
+
+    if (selectedProvider == null) {
+        if (providers.length > 0) {
+            const provider = providers[0];
+
+            setSelectedProvider(provider)
+
+            provider.provider
+                .request({ method: 'eth_requestAccounts' })
+                .then((accountsUntyped: unknown) => {
+                    let accounts = accountsUntyped as [string];
+
+                    if (accounts.length > 0) {
+                        setUserAccount(accounts[0]);
+                    }
+                })
+                .catch((error: any) => {
+                    console.log("Failed to connect to wallet: " + error);
+                });
+        }
+    }
+
+    if (userAccount !== null) {
+        let elements = React.Children.map(children, (child) => {
+            return React.cloneElement(child, { userAccount });
+        });
+
+        return (<>{elements}</>);
+    } else {
+        return (<>{children}</>);
+    }
+}
+
+export default AccountProvider;
