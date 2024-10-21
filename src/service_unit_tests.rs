b6d7f1c8041d723dfd638b3966648c8b2e5b8235
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2024-10-21 12:31:51 +0000

    Test if `checkEligibility` confirms eligibility
    Ensure that it returns `true` if the Space-and-Time query proves that
    the address is eligible.

--- src/service_unit_tests.rs
+++ src/service_unit_tests.rs
@@ -7,18 +7,74 @@ use airdrop_demo::{
     test_utils::{create_dummy_application_id, sign_claim},
     AirDropClaim,
 };
+use alloy_primitives::Address;
 use k256::ecdsa::SigningKey;
 use linera_sdk::{
     abis::fungible,
     base::{AccountOwner, ChainId, CryptoHash, Owner},
-    bcs, serde_json,
+    bcs, http, serde_json,
     service::MockServiceRuntime,
     util::BlockingWait,
     Service,
 };
 use rand::rngs::OsRng;
 
-use super::ApplicationService;
+use super::{ApplicationService, SXT_GATEWAY_URL};
+
+/// Tests if a GraphQL query can successfully check if an account is eligible.
+#[test]
+fn query_returns_address_is_eligible() {
+    let service = create_service();
+
+    let address = Address::random();
+    let api_token = "API token".to_owned();
+
+    let sql_query = format!(
+        "SELECT COUNT(*) FROM (SELECT * FROM ETHEREUM.NATIVE_WALLETS \
+        WHERE WALLET_ADDRESS = '0x{}' AND BALANCE > 0 LIMIT 1);",
+        hex::encode(address.as_slice())
+    );
+    let expected_query = format!(r#"{{ "sqlText": "{sql_query}" }}"#);
+
+    service
+        .runtime
+        .lock()
+        .expect("Test should abort on panic, so mutex should never be poisoned")
+        .add_expected_http_request(
+            http::Request::post(SXT_GATEWAY_URL, expected_query.as_bytes())
+                .with_header("Content-Type", b"application/json")
+                .with_header("Authorization", format!("Bearer {api_token}").as_bytes()),
+            http::Response::ok(b"[{ \"COUNT(1)\": 1 }]"),
+        );
+
+    let json_query = format!(
+        "{{ \"query\":
+            \"query {{ \
+                checkEligibility(address: \\\"{address}\\\", apiToken: \\\"{api_token}\\\") \
+            }}\"
+        }}"
+    );
+
+    let query = serde_json::from_str(&json_query).expect("Failed to deserialize GraphQL query");
+
+    let response = service.handle_query(query).blocking_wait();
+
+    assert_eq!(response.errors.len(), 0);
+
+    let async_graphql::Value::Object(data) = response.data else {
+        panic!("Unexpected response data: {response:?}");
+    };
+
+    assert_eq!(
+        data.len(),
+        1,
+        "Expected a single item in response data: {data:?}"
+    );
+    assert_eq!(
+        data["checkEligibility"],
+        async_graphql::Value::Boolean(true)
+    );
+}
 
 /// Tests if a GraphQL mutation can be used to create an [`AirDropClaim`] operation.
 #[test]
